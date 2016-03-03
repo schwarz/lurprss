31c0f3253e04035efb67fd971035c2670eced8a2
Bernhard Schwarz <bschwrz@gmail.com>
2016-03-03 21:07:02 +0100

    adapts hnbest for lurppis

--- hnbest.rb
+++ hnbest.rb
@@ -8,13 +8,12 @@ require "time"
 require "sequel"
 require "logger"
 
-HN_URI = "https://news.ycombinator.com"
-HNBEST_URI = "#{HN_URI}/best"
+LURPPIS_URI = "http://lurppis.tumblr.com/post/136246850348/my-articles"
 
 # 02 Oct 2002 15:00:00 +0200
 TIME_FORMAT = "%d %b %Y %H:%M:%S %z"  
 
-SELF_URI = "http://hnbest.herokuapp.com/rss"
+SELF_URI = "http://lurprss.herokuapp.com/rss"
 
 UPDATE_INTERVAL = 600
 
@@ -22,68 +21,40 @@ UPDATE_INTERVAL = 600
 ### DATABASE PART ###
 #####################
 
-DB = Sequel.connect(ENV['DATABASE_URL'] || "sqlite:///tmp/hnbest.db")
+DB = Sequel.connect(ENV['DATABASE_URL'] || "sqlite:///tmp/lurprss.db")
 DB.loggers << Logger.new($stdout)
 DB.create_table? :items do
   primary_key :id
   String :url, :null => false
   String :title, :null => false
-  Integer :points, :null => false
-  Integer :real_points, :null => false
-  String :user, :null => false
-  String :userurl, :null => false
-  String :commentsurl, :null => false
   DateTime :post_time, :null => false
   DateTime :last_seen_time, :null => false
 end
+
 DB.create_table? :last_update do
   primary_key :id
   DateTime :last_update, :null => false
 end
 
 def update_database
-  uri = URI.parse(HNBEST_URI)
+  uri = URI.parse(LURPPIS_URI)
   http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
   html = http.request(Net::HTTP::Get.new(uri.request_uri)).body
-
   doc = Nokogiri::HTML html
   
   items = DB[:items]
-  even = false
-  doc.css("td.title").each do |td|
-    if even
-      item = {}
       
-      item[:title] = td.text.strip
-      
-      item[:url] = td.css("a").first["href"]
-      if not item[:url].include? "://"
-        item[:url] = "#{HN_URI}/#{item[:url]}"
-      end
-      
-      td = td.parent.next_sibling.css("td.subtext").first
-      
-      item[:points] = td.css("span").first.text.split(" ").first.to_i
-      item[:real_points] = item[:points]
-      
-      item[:user] = td.css("a").first.text.strip
-      item[:userurl] = td.css("a").first["href"]
-      item[:userurl] = "#{HN_URI}/#{item[:userurl]}"
-      
-      item[:commentsurl] = td.css("a")[1]["href"]
-      item[:commentsurl] = "#{HN_URI}/#{item[:commentsurl]}"
-      
-      updated = items.filter(:url => item[:url]).update(:last_seen_time => Time.now, :real_points => item[:real_points])
-      if updated == 0
+  doc.css(".caption > p > a").each do |a|
+    item = {}
+
+    item[:title] = a.text.strip
+    item[:url] = a["href"]
+
+    updated = items.filter(:url => item[:url]).update(:last_seen_time => Time.now)
+    if updated == 0
         item[:post_time] = Time.now
         item[:last_seen_time] = Time.now
         items.insert(item)
-      end
-      
-      even = false
-    else
-      even = true
     end
   end
   
@@ -111,7 +82,7 @@ def fetch_items(count)
     update_database
   end
   
-  DB.from(DB[:items].order(Sequel.desc(:real_points)).limit(count).as(:posts)).order(Sequel.desc(:post_time)).all
+  DB.from(DB[:items].order(Sequel.desc(:post_time)).limit(count).as(:posts)).all
 end
 
 ####################
@@ -140,7 +111,7 @@ get "/rss" do
   items = fetch_items item_count
   lu = last_update
   haml :rss, :escape_html => true,
-       :locals => {:link => HNBEST_URI,
+       :locals => {:link => LURPPIS_URI,
                    :items => items,
                    :self_href => SELF_URI,
                    :last_build => lu,
@@ -155,46 +126,42 @@ __END__
 !!! 5
 %html
   %head
-    %title Hacker News Best RSS
-    %meta{:name => "keywords",
-          :content => "hacker, news, hackernews, rss, best"}
+    %title lurppis' Writing
     %link{:rel => "alternate",
           :type => "application/rss+xml",
-          :title => "Hacker News Best",
+          :title => "lurppis' Writing",
           :href => "/rss"}
   %body
     %h1
-      Hacker News Best
+      lurppis' Writing
       %a{:href => "/rss"} RSS
     %p
       You can append "?count=10" to reduce the amount of news items. The default is 30.
     %p
-      %a{:href => "https://github.com/kaini/hnbest"} Github
+      %a{:href => "https://github.com/schwarz/lurppis"} Github
+    %p
+      %a{:href => "https://github.com/kaini/hnbest"} Based on
 @@ rss
 !!! XML
 %rss{:version => "2.0", "xmlns:atom" => "http://www.w3.org/2005/Atom"}
   %channel
-    %title Hacker News Best
+    %title lurppis' Writing
     %link= link
     <atom:link href="#{self_href}" rel="self" type="application/rss+xml" />
-    %description This feed contains the Hacker News Best entries.
+    %description This feed contains lurppis' writing.
     %lastBuildDate= last_build.strftime(time_format)
     %language en
     -items.each do |item|
       %item
-        %title= item[:title]
+        %title
+          <![CDATA[
+          item[:title]
+          ]]>
         %link= item[:url]
         %guid= item[:url]
         %pubDate= item[:post_time].strftime(time_format)
         %description
           <![CDATA[
           %p
-            Started with
-            = item[:points]
-            points; by
-            %a{:href => item[:userurl]}= item[:user]
-          %p
-            %a{:href => item[:commentsurl]} Comments
+            %a{:href => item[:url]}= item[:title]
           ]]>
-      
-
